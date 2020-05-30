70aba7a5a565e63373af5a88da6a96ee98dd7e74
James Soper <c171437@hms.com>
2020-05-29 20:53:47 -0500

    git env changes

--- Terminal/git.env.cmd
+++ Terminal/git.env.cmd
@@ -14,6 +14,15 @@ GOTO //////////////////////////////////////////////////
 
 ::  SET HOME=%USERPROFILE%
 
+::         https://hmsholdings@dev.azure.com/hmsholdings/eCare/_git/eCare_API
+::         https://hmsholdings@dev.azure.com/hmsholdings/eCare/_git/eCare_UI
+SET repo-vsts=https://c171437:clludnrlhhuxft4wbtbg7ozdwrexlllzvovuk42f6lxkydnff3ra@dev.azure.com/hmsholdings/eCare/_git/eCare_
+
+
+::         https://github.com/hmsholdings/cav-ecare-api.git
+::         https://github.com/hmsholdings/cav-ecare-ui.git
+SET repo-gh=https://james-hms:cf0e4a90c04a757e1e7af91b4b2808be8fbbaf17@github.com/hmsholdings/cav-ecare-
+
 
 SET GITDIR=W:\Git
 SET PATH=%GITDIR%\cmd;%PATH%
