2af55a1a8d908803261fa8f0ad675865e6dc9219
James Soper <c171437@hms.com>
2020-09-01 14:04:27 -0500

    adding more git aliases

---
+++ gclean.cmd
@@ -0,0 +1,79 @@
+@ECHO OFF
+
+
+GOTO //////////////////////////////////////////////////
+
+usage: git [--version] [--help] [-C <path>] [-c <name>=<value>]
+           [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
+           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
+           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
+           <command> [<args>]
+
+These are common Git commands used in various situations:
+
+start a working area (see also: git help tutorial)
+   clone             Clone a repository into a new directory
+   init              Create an empty Git repository or reinitialize an existing one
+
+work on the current change (see also: git help everyday)
+   add               Add file contents to the index
+   mv                Move or rename a file, a directory, or a symlink
+   restore           Restore working tree files
+   rm                Remove files from the working tree and from the index
+   sparse-checkout   Initialize and modify the sparse-checkout
+
+examine the history and state (see also: git help revisions)
+   bisect            Use binary search to find the commit that introduced a bug
+   diff              Show changes between commits, commit and working tree, etc
+   grep              Print lines matching a pattern
+   log               Show commit logs
+   show              Show various types of objects
+   status            Show the working tree status
+
+grow, mark and tweak your common history
+   branch            List, create, or delete branches
+   commit            Record changes to the repository
+   merge             Join two or more development histories together
+   rebase            Reapply commits on top of another base tip
+   reset             Reset current HEAD to the specified state
+   switch            Switch branches
+   tag               Create, list, delete or verify a tag object signed with GPG
+
+collaborate (see also: git help workflows)
+   fetch             Download objects and refs from another repository
+   pull              Fetch from and integrate with another repository or a local branch
+   push              Update remote refs along with associated objects
+
+'git help -a' and 'git help -g' list available subcommands and some
+concept guides. See 'git help <command>' or 'git help <concept>'
+to read about a specific subcommand or concept.
+See 'git help git' for an overview of the system.
+
+
+::::::::::  git clean - Remove untracked files from the working tree
+
+git clean [-d] [-f] [-i] [-n] [-q] [-e <pattern>] [-x | -X] [--] <path>...
+
+Cleans the working tree by recursively removing files that are not under version control,
+ starting from the current directory.
+
+Normally, only files unknown to Git are removed, but if the -x option is specified,
+ ignored files are also removed. This can, for example, be useful to remove all build products.
+
+If any optional <path>... arguments are given, only those paths are affected.
+
+://////////////////////////////////////////////////
+
+if ""%1""=="""" GOTO howTO
+
+::  Remove untracked file, 1 by 1
+git clean -d -f %1
+
+GOTO END
+
+:howTO
+
+ECHO "usage: %0 <file>"
+
+:END
+
